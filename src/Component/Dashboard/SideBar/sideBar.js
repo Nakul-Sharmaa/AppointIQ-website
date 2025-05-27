dd7ffeef0640ca457c1044879b272c55e2b4626a
Nakul-Sharma <nakulsharma9625@gmail.com>
2025-05-27 21:18:16 +0530

    side modification

--- src/Component/Dashboard/SideBar/sideBar.js
+++ src/Component/Dashboard/SideBar/sideBar.js
@@ -5,7 +5,7 @@ export default function Sidebar( {setSelectedIndex}) {
   return (
     <div className="sidebar">
       <h2 className="logo">DAMS</h2>
-      <div className="profile-icon">👤</div>
+      
       <ul className='profile-ul'>
      {sideBarItems.map((item, index) => (
           <li key={index} onClick={() => setSelectedIndex(index)}>
